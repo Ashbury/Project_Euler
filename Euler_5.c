8841fe37a2344fdc655075d86edb29f720166c03
Ashbury711 <justasze@student.42.fr>
2017-07-26 14:02:29 +0200

    This solution is better : way less numbers to check

--- Euler_5.c
+++ Euler_5.c
@@ -5,20 +5,15 @@
 
 #include <stdio.h>
 
-/*
-** My implementation is naive, I admit it.
-*/
-
 int main(void)
 {
     unsigned int i = 2520; // Divisible by all numbers from 1 to 10.
 
     while (i += 20) // since 2520 is divisible by 20 and the final result has to be too.
     {
-        if (i % 2 == 0 && i % 3 == 0 && i % 4 == 0 && i % 5 == 0 && i % 6 == 0
-            && i % 7 == 0 && i % 8 == 0 && i % 9 == 0 && i % 10 == 0 && i % 11 == 0 
-            && i % 12 == 0  && i % 13 == 0 && i % 14 == 0 && i % 15 == 0  && i % 16 == 0 
-            && i % 17 == 0 && i % 18 == 0 && i % 19 ==0 && i % 20 == 0)
+        //I tried to take the least number of numbers possible to check
+        if (i % 3 == 0 && i % 5 == 0 && i % 7 == 0 && i % 11 == 0 && i % 13 == 0 
+            && i % 16 == 0 && i % 17 == 0 && i % 18 == 0 && i % 19 ==0 && i % 20 == 0)
         {
             printf("Result : %u\n", i);
             break ;
