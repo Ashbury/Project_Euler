56e9e688a33df5aab7b18a08599b5364e4929260
Ashbury <justasze@student.42.fr>
2018-02-05 16:32:02 +0100

    Data setup for problem 18.

--- Euler_18.c
+++ Euler_18.c
@@ -26,3 +26,45 @@
 **	 63 66 04 68 89 53 67 30 73 16 69 87 40 31
 **	04 62 98 27 23 09 70 98 73 93 38 53 60 04 23
 */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#define NB_ROWS 15
+#define NB_VALUE 120
+
+static int	**init_array(char *data)
+{
+	int		**ret;
+
+	ret = malloc(sizeof(int*) * NB_ROWS);
+	for (int i = 0; i < NB_ROWS; i++)
+	{
+		ret[i] = malloc(sizeof(int) * (i + 1));
+		if (i == 0)
+			ret[i][0] = atoi(strtok(data, " "));
+		else
+			for (int j = 0; j < (i + 1); j++)
+				ret[i][j] = atoi(strtok(NULL, " "));
+	}
+
+	return ret;
+}
+
+int		main(void)
+{
+	char	*data = strdup("75 95 64 17 47 82 18 35 87 10 20 04 82 47 65 19 01 23 75 03 34 88 02 77 73 07 63 67 99 65 04 28 06 16 70 92 41 41 26 56 83 40 80 70 33 41 48 72 33 47 32 37 16 94 29 53 71 44 65 25 43 91 52 97 51 14 70 11 33 28 77 73 17 78 39 68 17 57 91 71 52 38 17 14 91 43 58 50 27 29 48 63 66 04 68 89 53 67 30 73 16 69 87 40 31 04 62 98 27 23 09 70 98 73 93 38 53 60 04 23");
+	int		**array = init_array(data);
+	
+	for (int i = 0; i < NB_ROWS; i++)
+	{
+		for (int j = 0; j < (i + 1); j++)
+			printf("%d\n", array[i][j]);
+	}
+	for (int i = 0; i < NB_ROWS; i++)
+		free(array[i]);
+	free(array);
+	free(data);
+	return 0;
+}
